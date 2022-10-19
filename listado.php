352e36d157e2fed4ddfd41b50e7105c452a7c8b7
Samuel Angulo Alfonso <samuelaa2000@gmail.com>
2022-10-19 11:57:45 +0100

    actualizacion

--- listado.php
+++ listado.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Control de stock</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
@@ -20,31 +19,31 @@
         $arrDatos=$busqueda->fetchAll(PDO::FETCH_ASSOC);
         //print_r($arrDatos);
     ?>
-    <div class="container">
-        <div class="titulo">
+    <div class="titulo">
             <h1>Gestion de productos</h1>
-        </div>
-
+    </div>
+    
+    <div class="contenido">
         <div>
-        <a href="crear.php"><button type="button" class="btn btn-success">Crear</button></a>
-        <br>
-        <br>
-            <table class="table table-striped table-dark">
+            <a href="crear.php"><button type="button" class="success">Crear</button></a>
+            <br>
+            <br>
+            <table>
                 <tr>
-                    <th scope="col">Detalle</th>
-                    <th scope="col">Codigo</th>
-                    <th scope="col">Nombre</th> 
-                    <th scope="col">Acciones</th>
+                    <th>Detalle</th>
+                    <th>Codigo</th>
+                    <th>Nombre</th> 
+                    <th>Acciones</th>
                     <?php
                         foreach ($arrDatos as $muestra) {
                             echo '<tr>';
                     
-                            echo '<td scope="row"><a href=""><button type="button" class="btn btn-info">Detalle</button></a></td>';
-                            echo '<td scope="row">' . $muestra['id'] . '</td>';
-                            echo '<td scope="row">' . $muestra['nombre'] . '</td>';
-                            echo '<td scope="row">
-                                <a href=""><button type="button" class="btn btn-warning">Actualizar</button></a>
-                                <a href=""><button type="button" class="btn btn-danger">Eliminar</button></a>
+                            echo '<td><a href=""><button type="button" class="info">Detalle</button></a></td>';
+                            echo '<td>' . $muestra['id'] . '</td>';
+                            echo '<td>' . $muestra['nombre'] . '</td>';
+                            echo '<td>
+                                <a href=""><button type="button" class="warning">Actualizar</button></a>
+                                <a href=""><button type="button" class="danger">Eliminar</button></a>
                                 </td>';
                         }
                     ?>

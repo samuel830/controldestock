2cac47907a589a9b92e8e546e65c434a0b6ebc7a
Samuel Angulo Alfonso <samuelaa2000@gmail.com>
2022-10-28 13:41:33 +0100

    trabajando en muevestock.php

--- muevestock.php
+++ muevestock.php
@@ -9,6 +9,7 @@
 </head>
 <body>
     <?php
+    if(count($_GET) == 2){
         $nombreProducto = $_GET["nombre"];
         try{
             $conexion=new PDO("mysql:host=localhost;dbname=proyecto","samuel","1234");
@@ -57,29 +58,50 @@
 
                         foreach($arrTiendas as $tiendas){
                             echo '<tr>';
-                            echo '<form action="listado.php" method="get">';
-                            echo '<td>' . $tiendas['nombre'] . '</td>';    
-                            echo '<td>' . $muestra['unidades'] . '</td>';    
-                            echo '<td><select>';
+                            echo '<form action="muevestock.php" method="get">';
+                            echo '<td>' . $tiendas['nombre'] . '</td>';  
+                            echo '<input name="tiendaActual" type="hidden" value="'.$tiendas['id'].'">';  
+                            echo '<td>' . $muestra['unidades'] . '</td>';     
+                            echo '<td><select name="tiendaDestino">';
                             foreach($arrNombres as $nombres){
-                                echo'<option>'. $nombres['nombre'] .'</option>';
+                                echo'<option value="'.$nombres['id'].'">'.$nombres['nombre'].'</option>';
                             }
                             echo '</select></td>'; 
-                            echo '<td><select>';
+                            echo '<td><select name="unidadesEnviar">';
                             for($i = 1 ; $i <= $muestra['unidades']; $i++){
-                                echo '<option>'.$i.'</option>';
+                                echo '<option value="'.$i.'">'.$i.'</option>';
                             }
-                            echo '</select></td>';     
+                            echo '</select></td>';   
+                            echo '<input name="nombreProducto" type="hidden" value="'.$nombreProducto.'">';  
+                            echo '<input name="codigoProducto" type="hidden" value="'.$codigo.'">';  
                             echo '<td><button class="button-input" type="submit">Enviar stock</button></td>';       
                             echo '</form>';
                         }
                     }
-                ?>
+                    ?>
                 </tr>
                 <tr>
             </table>
+            <br><br>
+            <a href="listado.php"><button type="button" class="volver">Volver</button></a>
         </div>       
     </div>
+    <?php
+        }else if(count($_GET) > 2){
+            $tiendaActual = $_GET['tiendaActual'];
+            $tiendaDestino = $_GET['tiendaDestino'];
+            $unidades = $_GET['unidadesEnviar'];
+            $nombreProducto  = $_GET['nombreProducto'];
+            $codigoProducto  = $_GET['codigoProducto'];
+
+            if($tiendaActual != $tiendaDestino){
+                echo "Se va a enviar $unidades de $nombreProducto desde $tiendaActual a $tiendaDestino";
+                //$actualizacion = $conexion->query("UPDATE stocks set unidades");
+            }else{
+                echo "Se esta intentando enviar el producto a la misma tienda";
+            }           
+        }
+     ?>
 </body>
 </html>
 
